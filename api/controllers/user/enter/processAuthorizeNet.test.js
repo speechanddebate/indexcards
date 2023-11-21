5cc2dfc0cc9d4e6c3cff436fb266b4e577b53e19
Aaron Hardy <ashtarcommunications@gmail.com>
2023-11-21 10:33:01 -0700

    Authorize.net endpoint sorta working

---
+++ api/controllers/user/enter/processAuthorizeNet.test.js
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { assert } from 'chai';
+import server from '../../../../app';
+
+// eslint-disable-next-line jest/no-disabled-tests
+describe.skip('Authorize.net', () => {
+	it('Processes an authorize.net payment', async () => {
+		const body = {
+			tourn: 24674,
+			tourn_name: 'Share Test',
+			school: 13811,
+			school_name: 'Hardy Test',
+			customerInformation: {
+				firstName: 'Test',
+				lastName: 'User',
+			},
+			opaqueData: {
+				dataDescriptor: 'COMMON.ACCEPT.INAPP.PAYMENT',
+				dataValue: 'eyJjb2RlIjoiNTBfMl8wNjAwMDUzQTIxMjNDNDU1NTZDNzA1MTE0NkZCNUVBM0E4NTM3OUY2RDRDNzk4MTZBMjNENjUxMzg2M0Y0RjNERkM4RTY4Q0UxN0QwMTNFODU2N0FFRTA5MjMxQjcxMEEzM0QyNzRBIiwidG9rZW4iOiI5NzAwNTg3MTA4MDM2Njc5ODA0NjAxIiwidiI6IjEuMSJ9',
+			},
+			base: 10,
+		};
+		const res = await request(server)
+			.post(`/v1/user/enter/authorize`)
+			.set('Accept', 'application/json')
+			.send(body)
+			.expect('Content-Type', /json/)
+			.expect(200);
+
+		assert.isObject(res.body, 'Response is an object');
+	}, 30000);
+});
