ec32624ba0d8ed8d82390fec3d67181d8984ba0c
Aaron Hardy <ashtarcommunications@gmail.com>
2024-03-05 12:04:51 -0700

    Order

--- api/helpers/mail.js
+++ api/helpers/mail.js
@@ -51,10 +51,10 @@ export const emailBlast = async (inputData) => {
 	// Only send BCC emails so folks do not reply all or see student contact
 	// etc. And then add the sender as the To as well so it will not deliver.
 
-	messageData.replyTo = messageData.replyTo || messageData.from || config.MAIL_FROM;
 	messageData.bcc = Array.from(new Set(messageData.email));
 	messageData.to = messageData.to || config.MAIL_FROM;
 	messageData.from = messageData.from || config.MAIL_FROM;
+	messageData.replyTo = messageData.replyTo || messageData.from;
 	messageData.subject = messageData.subject || 'Message from Tab';
 
 	if (messageData.text) {
