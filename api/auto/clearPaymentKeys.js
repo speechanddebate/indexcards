4dfeb3eb382fc34eae4d9ca88bfd86b38884235b
Chris Palmer <palmer@tabroom.com>
2024-03-26 14:49:22 -0400

    Linting.  Payment keys clearing.  Dashboard bug.  Exports for testing

---
+++ api/auto/clearPaymentKeys.js
@@ -0,0 +1,24 @@
+import db from '../helpers/litedb.js';
+
+export const clearPaymentKeys = async () => {
+
+	await db.sequelize.query(`
+		DELETE TS FROM tourn_setting TS    
+		INNER JOIN tourn T ON T.id = TS.tourn    
+		WHERE     
+			T.end < NOW() - INTERVAL 7 DAY
+			AND (TS.tag LIKE '%authorizenet%' 
+				OR TS.tag LIKE '%paypal%'
+				OR TS.tag LIKE '%stripe%'
+			)    
+	`, {
+		type : db.sequelize.QueryTypes.DELETE,
+	});
+
+	return 'Paypal/Authorize/Stripe Payment keys deleted for tournaments more than a week over.';
+};
+
+await clearPaymentKeys();
+process.exit();
+
+export default clearPaymentKeys;
