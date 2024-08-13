80ebae60283fb01c60ffe32dbfe67d41a9bb3e24
Chris Palmer <palmer@tabroom.com>
2024-08-13 15:00:18 -0400

    String not JSON on the push notify key

--- api/models/session.js
+++ api/models/session.js
@@ -4,25 +4,29 @@ const session = (sequelize, DataTypes) => {
 			type: DataTypes.STRING(127),
 			allowNull: false,
 		},
-		ip: {
-			type: DataTypes.STRING(63),
-			allowNull: false,
+		ip            : {
+			type      : DataTypes.STRING(63),
+			allowNull : false,
 		},
-		defaults: {
-			type: DataTypes.JSON,
-			allowNull: true,
+		defaults      : {
+			type      : DataTypes.JSON,
+			allowNull : true,
 		},
-		agent_data: {
-			type: DataTypes.JSON,
-			allowNull: true,
+		agent_data    : {
+			type      : DataTypes.JSON,
+			allowNull : true,
 		},
-		geoip: {
-			type: DataTypes.JSON,
-			allowNull: true,
+		geoip         : {
+			type      : DataTypes.JSON,
+			allowNull : true,
 		},
-		push_notify: {
-			type: DataTypes.JSON,
-			allowNull: true,
+		push_notify   : {
+			type      : DataTypes.STRING(127),
+			allowNull : true,
+		},
+		last_access   : {
+			type      : DataTypes.DATE,
+			allowNull : true,
 		},
 	});
 };
