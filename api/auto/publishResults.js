79d596fe320254df837e02b9a493ddf606d070db
Chris Palmer <palmer@tabroom.com>
2024-11-21 15:11:54 -0500

    Removed a dead lock creator from autopublish

--- api/auto/publishResults.js
+++ api/auto/publishResults.js
@@ -3,23 +3,28 @@ import db from '../helpers/litedb.js';
 const autoPublishResults = async () => {
 
 	await db.sequelize.query(`
-		update round, event_setting autopublish
+		update round, event_setting autopublish, event, tourn
 			set round.post_primary = 3
 		where round.event = autopublish.event
+			and autopublish.event = event.id
+			and event.tourn = tourn.id
+			and tourn.start < CURRENT_TIMESTAMP
+			and tourn.end > (NOW() + INTERVAL 1 DAY)
 			and autopublish.tag = 'autopublish_results'
 			and autopublish.value > 0
-			and round.start_time > NOW() - INTERVAL 1 DAY
+			and round.start_time > (NOW() - INTERVAL 1 DAY)
 			and (round.post_primary != 3 OR round.post_primary IS NULL)
 
-		and not exists (
-			select ballot.id
-				from ballot, panel
-			where ballot.panel = panel.id
-				and panel.round = round.id
-				and ballot.audit != 1
-				and ballot.bye != 1
-				and panel.bye != 1
-		)
+        and (
+            select COUNT(b1.id)
+            from ballot b1, panel p1
+            where p1.round = round.id
+            and p1.id = b1.panel
+            and p1.bye = 0
+            and b1.bye = 0
+            and b1.forfeit = 0
+            and b1.audit = 0
+        ) = 0
 
 		and exists (
 			select b2.id
@@ -41,23 +46,29 @@ const autoPublishResults = async () => {
 	});
 
 	await db.sequelize.query(`
-		update round, event_setting autopublish
+		update round, event_setting autopublish, event, tourn
 			set round.post_feedback = 2
 		where round.event = autopublish.event
+			and autopublish.event = event.id
+			and event.tourn = tourn.id
+			and tourn.start < CURRENT_TIMESTAMP
+			and tourn.end > (NOW() + INTERVAL 1 DAY)
 			and autopublish.tag = 'autopublish_results'
 			and autopublish.value > 1
-			and round.start_time > NOW() - INTERVAL 1 DAY
+			and round.start_time > (NOW() - INTERVAL 1 DAY)
 			and (round.post_feedback != 2 OR round.post_feedback IS NULL)
 
-		and not exists (
-			select ballot.id
-				from ballot, panel
-			where ballot.panel = panel.id
-				and panel.round = round.id
-				and ballot.audit != 1
-				and ballot.bye != 1
-				and panel.bye != 1
-		)
+        and (
+            select COUNT(b1.id)
+            from ballot b1, panel p1
+            where p1.round = round.id
+            and p1.id = b1.panel
+            and p1.bye = 0
+            and b1.bye = 0
+            and b1.forfeit = 0
+            and b1.audit = 0
+        ) = 0
+
 		and exists (
 			select ballot.entry
 				from ballot, panel, round r2
@@ -71,23 +82,29 @@ const autoPublishResults = async () => {
 	});
 
 	await db.sequelize.query(`
-		update round, event_setting autopublish
+		update round, event_setting autopublish, event, tourn
 			set round.post_secondary = 3
 		where round.event = autopublish.event
+			and autopublish.event = event.id
+			and event.tourn = tourn.id
+			and tourn.start < CURRENT_TIMESTAMP
+			and tourn.end > (NOW() + INTERVAL 1 DAY)
 			and autopublish.tag = 'autopublish_results'
 			and autopublish.value  = '2'
-			and round.start_time > NOW() - INTERVAL 1 DAY
+			and round.start_time > (NOW() - INTERVAL 1 DAY)
 			and (round.post_secondary != 3 OR round.post_secondary IS NULL)
 
-		and not exists (
-			select ballot.id
-				from ballot, panel
-			where ballot.panel = panel.id
-				and panel.round = round.id
-				and ballot.audit != 1
-				and ballot.bye != 1
-				and panel.bye != 1
-		)
+        and (
+            select COUNT(b1.id)
+            from ballot b1, panel p1
+            where p1.round = round.id
+            and p1.id = b1.panel
+            and p1.bye = 0
+            and b1.bye = 0
+            and b1.forfeit = 0
+            and b1.audit = 0
+        ) = 0
+
 		and exists (
 			select ballot.entry
 				from ballot, panel, round r2
