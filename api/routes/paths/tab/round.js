0fceab32fe6818cdc12e7c06738beadb63150f91
Chris Palmer <palmer@tabroom.com>
2024-11-25 07:33:43 -0500

    Name names for reboots and expansions.  Do not break the rules.

--- api/routes/paths/tab/round.js
+++ api/routes/paths/tab/round.js
@@ -6,14 +6,14 @@ import { makeShareRooms } from '../../../controllers/tab/round/share.js';
 import { mergeTimeslotRounds, unmergeTimeslotRounds } from '../../../controllers/tab/round/merge.js';
 
 export default [
-	{ path : '/tab/{tournId}/round/{roundId}/sidecount'      , module : sideCounts }            ,
-	{ path : '/tab/{tournId}/round/{roundId}/status'         , module : roundDecisionStatus }   ,
-	{ path : '/tab/{tournId}/round/{roundId}/log'            , module : roundChangeLog }        ,
-	{ path : '/tab/{tournId}/round/{roundId}/blast'          , module : blastRoundPairing }     ,
-	{ path : '/tab/{tournId}/round/{roundId}/message'        , module : blastRoundMessage }     ,
-	{ path : '/tab/{tournId}/round/{roundId}/dashboard'      , module : tournDashboard }        ,
 	{ path : '/tab/{tournId}/round/{roundId}/attendance'     , module : tournAttendance }       ,
+	{ path : '/tab/{tournId}/round/{roundId}/blast'          , module : blastRoundPairing }     ,
+	{ path : '/tab/{tournId}/round/{roundId}/dashboard'      , module : tournDashboard }        ,
+	{ path : '/tab/{tournId}/round/{roundId}/log'            , module : roundChangeLog }        ,
 	{ path : '/tab/{tournId}/round/{roundId}/makeShareRooms' , module : makeShareRooms }        ,
 	{ path : '/tab/{tournId}/round/{roundId}/merge'          , module : mergeTimeslotRounds }   ,
+	{ path : '/tab/{tournId}/round/{roundId}/message'        , module : blastRoundMessage }     ,
+	{ path : '/tab/{tournId}/round/{roundId}/sidecount'      , module : sideCounts }            ,
+	{ path : '/tab/{tournId}/round/{roundId}/status'         , module : roundDecisionStatus }   ,
 	{ path : '/tab/{tournId}/round/{roundId}/unmerge'        , module : unmergeTimeslotRounds } ,
 ];
