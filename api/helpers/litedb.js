7104a5230f888b6eef6795ed32e08fc3e862f083
Chris Palmer <palmer@tabroom.com>
2024-12-17 15:46:49 -0500

    Backports ahoy

--- api/helpers/litedb.js
+++ api/helpers/litedb.js
@@ -21,4 +21,40 @@ export const db = {
 	Sequelize,
 };
 
+export const massUpdate = async (updateQuery, rawIds, key = 'id') => {
+
+	const cleanIds = rawIds.map( (raw) => { return raw[key]; });
+	const allArrays = [];
+	let idArray = [];
+
+	for (const idTick of cleanIds) {
+		if (idArray.length > 4) {
+			allArrays.push(idArray);
+			idArray = [];
+		}
+		idArray.push(idTick);
+	}
+
+	if (idArray.length) {
+		allArrays.push(idArray);
+	}
+
+	const promises = [];
+
+	allArrays.forEach( (keys) => {
+		console.log(`Updater has ${keys.length} keys to update ${JSON.stringify(keys)}`);
+		const updatePromise = db.sequelize.query(
+			updateQuery,
+			{
+				type         : db.sequelize.QueryTypes.UPDATE,
+				replacements : { keys },
+			}
+		);
+		promises.push(updatePromise);
+	});
+
+	await Promise.all(promises);
+	return 'Updates complete';
+};
+
 export default db;
