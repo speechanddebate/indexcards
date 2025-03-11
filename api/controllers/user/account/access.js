3a5481a9e1dab1edcf4b05c6026851d550935f26
Chris Palmer <palmer@tabroom.com>
2025-03-11 11:34:47 -0400

    Minor tweak to prevent deadlocks

--- api/controllers/user/account/access.js
+++ api/controllers/user/account/access.js
@@ -2,15 +2,15 @@ export const updateLastAccess = {
 
 	GET: async (req, res) => {
 
-		if (req.session.su) {
-			return res.status(200).json('OK');
+		if (req.session.su > 0) {
+			return res.status(200).json('OK - Update skipped due to SU session');
 		}
 
-		const last = Date.parse(req.session.last_access);
-		const now  = new Date();
-		const then = now.setDate(now.getDate() - 1);
+		const last = Date.parse(`${req.session.last_access}Z`);
+		const dateLimit  = new Date();
+		dateLimit.setHours(dateLimit.getHours() - 2 );
 
-		if ( Number.isNaN(last) || last < then) {
+		if ( Number.isNaN(last) || last < dateLimit) {
 			await req.db.sequelize.query(`
 				update session set last_access = NOW()
 				where session.id = :sessionId
