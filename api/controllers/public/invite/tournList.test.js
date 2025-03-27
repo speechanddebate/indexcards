d9c53196fbf7208250fbe124a6e8f472927dfa6f
Chris Palmer <palmer@tabroom.com>
2025-03-27 11:20:07 -0400

    list and tests

--- api/controllers/public/invite/tournList.test.js
+++ api/controllers/public/invite/tournList.test.js
@@ -17,14 +17,14 @@ describe('Tournament Front Listing', () => {
 
 		const body = res.body;
 
-		assert.typeOf(body, 'array', 'Array returned');
-		assert.typeOf(body[0], 'object', 'Array contains objects');
+		assert.typeOf(body    , 'array'  , 'Array returned');
+		assert.typeOf(body[0] , 'object' , 'Array contains objects');
 
-		assert.equal(body.length, '10', 'I should have found eight tournaments.  Two are districts with 2 weekends each');
-		assert.typeOf(body[0].id   , 'number' , 'Object contains valid ID number');
-		assert.typeOf(body[0].year , 'number' , 'Object contains valid year');
-		assert.typeOf(body[0].sortweek , 'string' , 'Object contains valid week number');
-		assert.typeOf(body[0].name , 'string' , 'Object contains valid names');
-		assert.equal(body[0].webname, 'newengland', `The first listed should be the New England district`);
+		assert.equal(body.length          , '10'         , 'I should have found eight tournaments.  Two are districts with 2 weekends each');
+		assert.typeOf(body[0].tournId     , 'number'     , 'Object contains valid ID number');
+		assert.typeOf(body[0].year        , 'number'     , 'Object contains valid year');
+		assert.typeOf(body[0].sortnumeric , 'number'     , 'Object contains valid week number');
+		assert.typeOf(body[0].name        , 'string'     , 'Object contains valid names');
+		assert.equal(body[0].webname      , 'newengland' , `The first listed should be the New England district`);
 	});
 });
