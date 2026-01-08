19ec7df77851eed55ea7ad38ed64956b28878cc1
Ryan Tullis <tulliscryan@gmail.com>
2026-01-08 15:40:32 -0600

    add a few easy ones now

--- api/routes/routers/v1/pages/pagesRouter.js
+++ api/routes/routers/v1/pages/pagesRouter.js
@@ -1,8 +1,5 @@
 import { Router } from 'express';
-import * as tournController from '../../../../controllers/rest/tournController.js';
 
 const router = Router();
-
-router.get('/invite/tourn/:tournId', tournController.getTournInvite);
 
 export default router;
